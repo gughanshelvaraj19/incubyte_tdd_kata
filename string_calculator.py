2e2fec0371db9cdcbee098e9358c4a9b0c2670f7
gughanshelvaraj19 <gughanshelvaraj@gmail.com>
2025-03-05 18:46:55 +0530

    Enhances add with type check to ensure correct input data

--- string_calculator.py
+++ string_calculator.py
@@ -5,13 +5,18 @@ class NegativeNumberException(Exception):
 def add(numbers: str) -> int:
     """
     String calculator to calculate the sum of all the integers in the string
-    :param numbers: string of numbers with delimiter
-    :return: sum of all the integers in the string
+    :param numbers: a string of delimiter-separated numbers
+    :return: an integer, sum of the numbers
     :raises NegativeNumberException: raises an exception
     """
     numbers_sum = 0
+    if not isinstance(numbers, str):
+        return numbers_sum
     return numbers_sum
 
 
 # test cases
 assert add("") == 0
+assert add(1) == 0
+assert add((1,)) == 0
+assert add([1,2]) == 0
